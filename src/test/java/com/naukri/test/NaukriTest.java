604f0f2ed52d6fc55d5dd4a3b6782c0feb7c41fe
Chinna.Putha <chinna.putha@shell.com>
2022-02-27 08:49:14 +0530

    Naukri test changes

--- src/test/java/com/naukri/test/NaukriTest.java
+++ src/test/java/com/naukri/test/NaukriTest.java
@@ -7,25 +7,23 @@ import com.naukri.pages.NaukriLogin;
 import com.naukri.pages.NaukriUpdate;
 import com.naukri.utils.TestBase;
 
-public class NaukriTest  extends TestBase{
-	
+public class NaukriTest extends TestBase {
+
 	@Test(alwaysRun = true)
 	public void lanuchNaukriApp() {
 		openApplication("chrome", "https://www.naukri.com/nlogin/login");
 	}
-	
+
 	@Test(dependsOnMethods = "lanuchNaukriApp")
 	public void loginToNaukri() {
 		NaukriLogin nl = PageFactory.initElements(driver, NaukriLogin.class);
-		nl.loginNaukri("javacampus.selenium@gmail.com", "Testing@1234",driver);
+		nl.loginNaukri("javacampus.selenium@gmail.com", "Testing@1234", driver);
 	}
-	
-	/*
-	 * @Test(dependsOnMethods = "loginToNaukri") public void updateProfile() throws
-	 * InterruptedException { NaukriUpdate nu = PageFactory.initElements(driver,
-	 * NaukriUpdate.class); nu.updateMyProfile(driver); }
-	 */
-	
-	
+
+	@Test(dependsOnMethods = "loginToNaukri")
+	public void updateProfile() throws InterruptedException {
+		NaukriUpdate nu = PageFactory.initElements(driver, NaukriUpdate.class);
+		nu.updateMyProfile(driver);
+	}
 
 }
